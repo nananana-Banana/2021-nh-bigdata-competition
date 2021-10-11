2483e606ccc6a789b3d9e2a603e5367791142f15
Ahjeong Park <qkrdkwjd59@naver.com>
2021-10-11 17:37:01 +0900

    add features and external data

--- core/config.py
+++ core/config.py
@@ -1,2 +1,3 @@
 data_path = '~/2021-nh-bigdata-competition/data/'
 external_path = '~/2021-nh-bigdata-competition/external/result/'
+model_path = '~/2021-nh-bigdata-competition/model/notebooks/result/'
